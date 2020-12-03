a6e7c2023e0acfee6b257332f9afd692469fb41f
Ruaridh MacDowall <ruaridh@Ruaridhs-MacBook-Pro.local>
2020-12-03 19:41:05 +0000

    Updates

--- precache-manifest.074afd6a0802a08445014967274f9e78.js
+++ precache-manifest.074afd6a0802a08445014967274f9e78.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "747bf0fa859d2b5515d392126a79c260",
+    "revision": "d3ccf08bbdd5f548d8922939b0888266",
     "url": "/sorting_visualiser/index.html"
   },
   {
-    "revision": "ccbc7ec4129521d07d4e",
-    "url": "/sorting_visualiser/static/css/main.f0a5cef0.chunk.css"
+    "revision": "83059acc7d947efa5df1",
+    "url": "/sorting_visualiser/static/css/main.73acdc61.chunk.css"
   },
   {
     "revision": "ae4e3fa89f350a97ec16",
     "url": "/sorting_visualiser/static/js/2.6d8871a7.chunk.js"
   },
   {
-    "revision": "ccbc7ec4129521d07d4e",
-    "url": "/sorting_visualiser/static/js/main.4ca526a2.chunk.js"
+    "revision": "83059acc7d947efa5df1",
+    "url": "/sorting_visualiser/static/js/main.daa20032.chunk.js"
   },
   {
     "revision": "5b6870553593a5480b31",
